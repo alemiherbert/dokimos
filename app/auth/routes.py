c9f76bd6297c36f9501c1e23433cff0032eca130
Alemi Herbert <alemiherbert@gmail.com>
2024-07-16 21:31:58 +0300

    Add authentication template and routes, no functionality yet

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -6,3 +6,8 @@ from flask import render_template
 @auth.route("/login")
 def login():
     return render_template("auth/login.html")
+
+
+@auth.route("/signup")
+def signup():
+    return render_template("auth/signup.html")
